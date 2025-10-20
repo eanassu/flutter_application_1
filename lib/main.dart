3eccd86a5414eadc3dd67ac7fa640f9cf0dd3e2d
eanassu <eanassu@gmail.com>
2025-10-20 19:35:35 -0300

    telas usadas em outros exemplos

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/desenho.dart';
-import 'package:flutter_application_1/home_screen.dart';
+//import 'package:flutter_application_1/desenho.dart';
+//import 'package:flutter_application_1/home_screen.dart';
 import 'package:flutter_application_1/map_screen.dart';
 import 'package:flutter_application_1/segunda_tela.dart';
-import 'package:flutter_application_1/sensores.dart';
+//import 'package:flutter_application_1/sensores.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
